7fd79a8501a7ee5b19ca1f98740a9b8b7905cf2e
Newbie123-mino <adibsoffee@gmail.com>
2025-03-18 11:00:44 +0800

    Line12

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	string user, pass;
 	
-	cout << setw(50) << setfill('=') << "=" << "Welcome." << setw(50) << "=" << endl << endl;
+	cout << setw(50) << setfill('=') << "=" << "Welcome to Harith's shop." << setw(50) << "=" << endl << endl;
 	cout << setw(50) << setfill(' ') << " " << "**Profile Creation**" << endl;
 	cout << setw(50) << " " << "--------------------" << endl;
 	cout << setw(50) << " " << "Username : ";
